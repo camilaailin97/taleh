33e23f8eb9f9b82d06b6c47a77dee55b19abbc8a
Camila Ramos <camii_ailin@hotmail.com.ar>
2026-07-08 01:55:07 -0300

    cambios en total

--- js/carrito.js
+++ js/carrito.js
@@ -67,35 +67,30 @@ function eliminarProductoDelCarrito(id) {
 function obtenerSubtotalCarrito() {
     let subtotalTotal = 0;
     
-    // Agrupamos cantidades
     let grupos = { 'cruce-rosa': 0, 'espada': 0, 'set-hebreo': 0, 'set-urbano': 0, 'set-foil-varios': 0 };
-    let precioBase = {}; // Guardamos el precio unitario de referencia
+    let precioBase = { 'cruce-rosa': 0, 'espada': 0, 'set-hebreo': 0, 'set-urbano': 0, 'set-foil-varios': 0 };
 
     carrito.forEach(p => {
+        // Aseguramos que el precio sea siempre un número
+        let precio = Number(p.precio) || 0;
+        
         if (grupos.hasOwnProperty(p.categoria)) {
             grupos[p.categoria] += p.cantidad;
-            precioBase[p.categoria] = p.precio; // Capturamos el precio real
+            precioBase[p.categoria] = precio;
         } else {
-            subtotalTotal += p.precio * p.cantidad;
+            subtotalTotal += precio * p.cantidad;
         }
     });
 
-    // 🌟 Aplicamos lógica con precios dinámicos
-    // Ej: Cruces/Rosas (2 unidades = precio de 1 combo, o lo que definas)
-    // Nota: Como tus promos son complejas, aquí el cálculo toma el precio del producto
-    subtotalTotal += (Math.floor(grupos['cruce-rosa'] / 2) * (precioBase['cruce-rosa'] * 1.5)) + ((grupos['cruce-rosa'] % 2) * (precioBase['cruce-rosa'] || 900));
-    
-    subtotalTotal += (Math.floor(grupos['espada'] / 2) * (precioBase['espada'] * 1.8)) + ((grupos['espada'] % 2) * (precioBase['espada'] || 1000));
-    
-    subtotalTotal += (Math.floor(grupos['set-hebreo'] / 4) * (precioBase['set-hebreo'] * 2.6)) + ((grupos['set-hebreo'] % 4) * (precioBase['set-hebreo'] || 1500));
-
-    subtotalTotal += (Math.floor(grupos['set-urbano'] / 3) * (precioBase['set-urbano'] * 2.1)) + ((grupos['set-urbano'] % 3) * (precioBase['set-urbano'] || 1900));
-
-    subtotalTotal += (Math.floor(grupos['set-foil-varios'] / 3) * (precioBase['set-foil-varios'] * 2)) + ((grupos['set-foil-varios'] % 3) * (precioBase['set-foil-varios'] || 1700));
+    // Cálculos protegidos: usamos (precioBase[...] || 0) para evitar undefined
+    subtotalTotal += (Math.floor(grupos['cruce-rosa'] / 2) * (precioBase['cruce-rosa'] * 1.5)) + ((grupos['cruce-rosa'] % 2) * precioBase['cruce-rosa']);
+    subtotalTotal += (Math.floor(grupos['espada'] / 2) * (precioBase['espada'] * 1.8)) + ((grupos['espada'] % 2) * precioBase['espada']);
+    subtotalTotal += (Math.floor(grupos['set-hebreo'] / 4) * (precioBase['set-hebreo'] * 2.6)) + ((grupos['set-hebreo'] % 4) * precioBase['set-hebreo']);
+    subtotalTotal += (Math.floor(grupos['set-urbano'] / 3) * (precioBase['set-urbano'] * 2.1)) + ((grupos['set-urbano'] % 3) * precioBase['set-urbano']);
+    subtotalTotal += (Math.floor(grupos['set-foil-varios'] / 3) * (precioBase['set-foil-varios'] * 2)) + ((grupos['set-foil-varios'] % 3) * precioBase['set-foil-varios']);
 
     return Math.round(subtotalTotal);
 }
-
 function vaciarCarrito() {
     carrito = [];
     guardarCarritoEnStorage();
